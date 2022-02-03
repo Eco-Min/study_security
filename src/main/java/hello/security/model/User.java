5d16476ce782b2f0d3b1a951066dd8d33e65a9f9
Eco_min <guldor10.b@gmail.com>
2022-02-03 17:26:30 +0900

    oauth 발급 위치 및 ID 설정(겹치지 않게 하기 위해)

--- src/main/java/hello/security/model/User.java
+++ src/main/java/hello/security/model/User.java
@@ -18,6 +18,12 @@ public class User {
     private String email;
     @Enumerated(EnumType.STRING)
     private Role role;
+
+    // oauth2 에서 받은 정보를 가지고 만들예정
+    // oauth2를 발급해주는 곳
+    private String provider;
+    // {sub}
+    private String providerId;
     @CreationTimestamp
     private LocalDateTime createDate;
 }
