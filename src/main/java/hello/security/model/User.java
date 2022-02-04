4dc81e8f077f120fc97576aeba2b3278198fe084
Eco_min <guldor10.b@gmail.com>
2022-02-04 15:57:37 +0900

    Oauth2 발급시 자동 회원가입

--- src/main/java/hello/security/model/User.java
+++ src/main/java/hello/security/model/User.java
@@ -1,6 +1,7 @@
 package hello.security.model;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
@@ -9,6 +10,7 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "user")
 @Data
+@NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue
@@ -20,10 +22,19 @@ public class User {
     private Role role;
 
     // oauth2 에서 받은 정보를 가지고 만들예정
-    // oauth2를 발급해주는 곳
+    // oauth2를 발급해주는 곳 getRegistrationId
     private String provider;
-    // {sub}
+    // {각사 id 와 결합}
     private String providerId;
     @CreationTimestamp
     private LocalDateTime createDate;
+
+    public User(String username, String password, String email, Role role, String provider, String providerId) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+        this.provider = provider;
+        this.providerId = providerId;
+    }
 }
